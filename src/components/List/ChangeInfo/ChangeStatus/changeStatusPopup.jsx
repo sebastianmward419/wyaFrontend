c7ae6607023e637bea64a58c83d0c3d41828bcd3
SEBASTIAN WARD <sebastian.ward1@ibm.com>
2020-02-28 11:02:16 -0500

    popup appears

---
+++ src/components/List/ChangeInfo/ChangeStatus/changeStatusPopup.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const changeStatus = color => {
+   // fetch ('')
+}
+
+const ChangeStatusPopup = props => (
+    <div style={formStyles}>
+        yea
+    </div>
+)
+
+export default ChangeStatusPopup;
+
+const formStyles = 
+{
+    div:
+    {
+        boxShadow: '5px 10px 8px 10px#888888',
+        width: '150px',
+        height: '150px',
+        padding: '20px',
+    }
+}
