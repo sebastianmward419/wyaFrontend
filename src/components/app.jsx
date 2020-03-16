fe823d292750d37aec1e2ff8e2ac888de64b4c39
Ryan Tong <ryan.tong@ibm.com>
2020-03-16 14:53:07 -0700

    unfinished changes to group view, don't push into master

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,35 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import HeaderMain  from './Header/header_main.jsx';
 import List        from './List/list.jsx';
 import Group       from './Group/group.jsx';
 import AddButton   from './List/AddUser/addButton.jsx';
 
-class App extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = { showGroup: false }
-    }
+const App = props => {
+    const [group, showGroup] = useState (false);
+    
+    useEffect (() => document.body.style.margin = 0)
 
-    componentDidMount () {
-        document.body.style.margin = 0;
-    }
-
-    render = () => (
-        <div>
-            <HeaderMain />
-            <List />
-            {this.state.showGroup && <Group />}
-            <AddButton />
+    return (
+        <div>ß
+        <HeaderMain />
+        {!this.state.showGroup && <List />}
+        {!this.state.showGroup && <AddButton />}
+        {this.state.showGroup && <Group />}
         </div>
     )
-
-    static triggerShowGroup = () => {
-        console.log('test');
-        console.log(this.state);
-        console.log(this.state.showGroup);
-        this.state.showGroup = true;
-    }
 }
 
 export default App;
